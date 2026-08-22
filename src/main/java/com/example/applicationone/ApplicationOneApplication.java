fae196718a9c9a4e16977c1a5ee6bcd2f71ac0c2
Sahana Ghosh <69307827+saghosh8@users.noreply.github.com>
2026-08-22 10:27:41 +0530

    creating application One

---
+++ src/main/java/com/example/applicationone/ApplicationOneApplication.java
@@ -0,0 +1,12 @@
+package com.example.applicationone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationOneApplication.class, args);
+    }
+}
